cfdb815c8050aa1a15be4329dcf51a588ed86b55
Peio <pierreroth64@gmail.com>
2016-02-01 17:27:21 +0100

    Add script on data types

---
+++ examples/02-data-types/data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+first = "Pierre" # double quote
+
+last = 'Roth' # single quote
+
+cv = """
+     Mon CV 
+     sur 
+     plusieurs lignes
+     """
+
+contact = '''
+          pierreroth4@gmail.com
+          @pierreroth64
+          '''
+
+print first + " " + last
+print
+print "Parcours:"
+print "--"
+print cv
+
+print "Contact:\n--"     # tiens tiens... un commentaire dans le code
+                         # qui explique un print avec caractere special 'line feed' (\n)
+print contact
