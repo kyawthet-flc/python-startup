957ded70dea44d6513c716e17351c3c246971f1c
Peio <pierreroth64@gmail.com>
2016-02-02 22:21:49 +0100

    Add list comprehension example

---
+++ examples/03-exec-flow/03-exec-listcomprehension.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# Non pythonic way (C-like)
+numbers = [2, 3, 7, 8, 90, 8, 8, 8]
+odds = []
+
+for number in numbers:
+    if number % 2:
+        odds.append(number)
+print "odds (c-developer):", odds
+
+# Pythonic way using list-comprehension
+odds = [number for number in numbers if number % 2]
+print "odds (pythonista!):", odds
+
+# You can do it with list of dicts
+AGE_LIMIT = 49
+people = [
+	{'name': 'Manu', 'age': 35},
+	{'name': 'Cedric', 'age': 40},
+	{'name': 'Diego', 'age': 56} ]
+
+old_ones = []
+for person in people:
+    if person['age'] > AGE_LIMIT:
+	old_ones.append(person['name'])
+print "old ones (c-developer):", old_ones
+
+# 1 line instead of 4!
+old_ones = [person['name'] for person in people if person['age'] > AGE_LIMIT]
+print "old ones (pythonista!):", old_ones
