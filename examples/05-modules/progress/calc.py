93c182109c3850a07baf53a71274afe23c2df6bf
Peio <pierreroth64@gmail.com>
2016-02-19 15:53:10 +0100

    Add progressbar example

---
+++ examples/05-modules/progress/calc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import time
+from progressbar import ProgressBar
+
+TIME_WINDOW_SECONDS = 3
+
+def calculate(loops):
+    sleep_value = float(TIME_WINDOW_SECONDS) / loops
+    pbar = ProgressBar().start()
+    for i in range(loops):
+        time.sleep(sleep_value)
+        # Do your stuff here!
+        pbar.update((i + 1) * 100 / loops)
+    pbar.finish()
+
+
+if __name__ == '__main__':
+    print("1% percent progression:")
+    calculate(100)
+    print("10% percent progression:")
+    calculate(10)
