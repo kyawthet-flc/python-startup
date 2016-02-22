30b41e1b9beae6d1638357762b60ef5bf20c91f0
Peio <pierreroth64@gmail.com>
2016-02-22 15:54:43 +0100

    Rework main file

--- examples/06-files/refactoring/process_file.py
+++ examples/06-files/refactoring/process_file.py
@@ -19,14 +19,14 @@ class FileProcessor(object):
         FileProcessor takes a data file as input and processes it
     """
 
-    def __init__(self, filename, notifiers=None, logger=None):
-        self.logger = logger if logger is not None else logging.getLogger("fileprocess")
+    def __init__(self, filename, notifiers=None):
+        self.logger = logging.getLogger("fileprocess")
         self.notifiers = notifiers if notifiers is not None else []
         self.decoder = self.get_decoder_from_file(filename)
 
     def get_decoder_from_file(self, filename):
 
-        file_ext = "json" # FIXME: decode extension
+        file_ext = os.path.splitext(filename)[1]
         for ext, decoder_class in SUPPORTED_FILE_DECODERS.items():
             if ext == file_ext:
                 return decoder_class(filename=filename, logger=self.logger)
@@ -41,14 +41,14 @@ class FileProcessor(object):
         return content
 
     def process(self):
-        data = self.decoder.get_data()
-        for employee in data['employees']:
-            self.logger.debug('printing employee: %s', )
-            pprint.pprint(employee)
+        employees = self.decoder.get_employees()
+        for employee in employees:
+            print('processing employee: %s...' % employee['name'])
             for notifier in self.notifiers:
                 notifier.notify(contact=employee['contact'], content=self._build_notif_msg(employee))
+            print('done.')
 
-def process_file(filename):
+def process_file():
 
     parser = OptionParser("usage: %prog [options] filename")
     parser.add_option("-d", "--debug", dest="debug_level",
@@ -66,12 +66,11 @@ def process_file(filename):
         level = logging.CRITICAL
 
     logging.basicConfig(level=level, format=LOG_FORMAT)
-    logger = logging.getLogger("readjson")
     notifiers = []
     notifiers.append(GmailNotifier());
 
     try:
-        processor = FileProcessor(filename=filename, notifiers=notifiers, logger=logger)
+        processor = FileProcessor(filename=filename, notifiers=notifiers)
         processor.process()
         sys.exit(0)
     except Exception as error:
@@ -83,4 +82,4 @@ def process_file(filename):
         sys.exit(-1)
 
 if __name__ == '__main__':
-    process_file('data.json')
+    process_file()
