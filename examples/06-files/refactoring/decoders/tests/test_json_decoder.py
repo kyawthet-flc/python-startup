3450716326e4139f1701f2f3cfd1b65541826dd7
Peio <pierreroth64@gmail.com>
2016-02-22 16:45:07 +0100

    Add test for JSON decoder

--- examples/06-files/refactoring/decoders/tests/test_json_decoder.py
+++ examples/06-files/refactoring/decoders/tests/test_json_decoder.py
@@ -5,11 +5,19 @@ from decoders.json_decoder import JSONFileDecoder
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def test_json_correct():
+    '''Test the decoding when a correct file is given'''
+
     decoder = JSONFileDecoder(os.path.join(CURRENT_DIR, 'correct.json'))
     assert len(decoder.get_employees()) == 3
 
 
 def test_json_bad_format():
+
     decoder = JSONFileDecoder(os.path.join(CURRENT_DIR, 'bad_format.json'))
     with pytest.raises(Exception):
         decoder.get_employees()
+
+def test_json_not_found():
+
+    with pytest.raises(Exception):
+        JSONFileDecoder(os.path.join(CURRENT_DIR, 'not_found.json'))
